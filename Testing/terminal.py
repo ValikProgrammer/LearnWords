41e07145792f474c2b59764a74a8aa966a9d4634
ValikProgrammer <100292757+ValikProgrammer@users.noreply.github.com>
2022-03-01 19:27:17 +0300

    Colors

--- Testing/terminal.py
+++ Testing/terminal.py
@@ -1,3 +1,13 @@
+INCORRECT = '\033[31m' # red
+CORRECT = '\033[32m' # green
+WARNING = '\033[33m'#'\033[33m' # orange
+BLUE = '\033[35m' # blue
+UNDERLINED = '\033[4m'
+BOLD = '\033[1m'
+END = '\033[0m' # simple text (stop colourful text)
+
+
+
 import os
 size = os.get_terminal_size()
 print(size)
